4c860747e6d539e2e13af96efc5cc4935b3cfe72
ki3nd <trungkienty2001@gmail.com>
2026-05-13 16:58:42 +0700

    chore: add MIT license

--- deepagents_chromafs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""deepagents-chromafs: ChromaDB-backed virtual filesystem for DeepAgents."""
-
-from deepagents_chromafs.backend import ChromaFsBackend
-from deepagents_chromafs.cache import ContentCache
-
-__all__ = ["ChromaFsBackend", "ContentCache"]
